6a06d112ba25bf835657f643adcc3fffc06cb858
nitishaMindrops <101164767+nitishaMindrops@users.noreply.github.com>
2022-03-15 11:40:50 +0530

    Add files via upload
    Updated Giphy App

--- GiphyWebApp/js/giphy.js
+++ GiphyWebApp/js/giphy.js
@@ -32,24 +32,26 @@ $(document).ready(function () {
 function TrendingScroll() {
 
     $("#ImageText").text("Trending Gifs");
-    $.ajax({
-        type: "GET",
-        url: "https://api.giphy.com/v1/gifs/trending?api_key=izV5X2YE3sY71qcdptseUwhja3ZFdKzv&offset=" + PageOffset + "&limit=10",
-        dataType: "json",
-        success: function (Imagedata) {
-            PageOffset = PageOffset + 10;
-            var offsetValue = Imagedata.pagination.offset;
-            if (!responseArray.includes(offsetValue)) {
+    var offsetValue = PageOffset;
+    if (offsetValue == 0 || (!responseArray.includes(offsetValue) && responseArray.includes(offsetValue - 10))) {
+        $.ajax({
+            type: "GET",
+            url: "https://api.giphy.com/v1/gifs/trending?api_key=izV5X2YE3sY71qcdptseUwhja3ZFdKzv&offset=" + PageOffset + "&limit=10",
+            dataType: "json",
+            success: function (Imagedata) {
+
                 for (var i = 0; i < Imagedata.data.length; i++) {
                     $("#dImg").append("<img src=\"" + Imagedata.data[i].images.original.url + "\"/>")
-                    responseArray.push(offsetValue);
-                }
-            }
-           // console.log(Imagedata);
 
-        }
-    });
-    //$("#dImg").empty();
+                }
+                responseArray.push(Imagedata.pagination.offset);
+
+            }
+        });
+    }
+    else {
+        PageOffset = PageOffset - 10;
+    }
 };
 
 
